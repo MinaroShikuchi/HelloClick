15e2214985efdd5a82b64617eebf9d6a98eb1c1d
Minaro <paturetromain@gmail.com>
2022-02-01 18:28:35 +0100

    Add fab

--- app/src/main/java/com/example/hellogames/MainActivity.kt
+++ app/src/main/java/com/example/hellogames/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hellogames
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlin.math.sign
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         val reverseButton : Button = findViewById(R.id.button3)
         val normalButton : Button = findViewById(R.id.button4)
         val bigButton : Button = findViewById(R.id.button5)
-        val nextButton : Button = findViewById(R.id.button6)
+        val nextButton : View = findViewById(R.id.fab)
 
         counterTextView.text = counter.toString()
 
