3beb948880e7383deb10fcb6282730528046493f
Minaro <paturetromain@gmail.com>
2022-02-01 17:36:54 +0100

    Add Counter activty

--- app/src/main/java/com/example/hellogames/MainActivity.kt
+++ app/src/main/java/com/example/hellogames/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.hellogames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+    private var counter : Int = 0
+    private var step : Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counterTextView : TextView = findViewById(R.id.textView3)
+        val clickMeButton : Button = findViewById(R.id.button2)
+        val resetButton : Button = findViewById(R.id.button)
+        val reverseButton : Button = findViewById(R.id.button3)
+
+        val normalButton : Button = findViewById(R.id.button4)
+        val bigButton : Button = findViewById(R.id.button5)
+
+        counterTextView.text = counter.toString()
+
+        clickMeButton.setOnClickListener {
+            counter = counter.plus(step)
+            counterTextView.text = counter.toString()
+        }
+
+        resetButton.setOnClickListener {
+            counter = 0
+            step = 1
+            counterTextView.text = counter.toString()
+        }
+
+        reverseButton.setOnClickListener {
+            step = step.times(-1)
+        }
+
+        normalButton.setOnClickListener {
+            step = step.sign * 1
+        }
+
+        bigButton.setOnClickListener {
+            step = step.sign.times(2)
+        }
+
     }
 }
